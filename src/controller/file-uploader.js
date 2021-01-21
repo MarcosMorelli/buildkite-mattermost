3a06d53eb6fe18ae2958db461d8f1255c3c9debd
Morelli <marcos.morelli@involves.com>
2021-01-21 17:11:47 -0300

    protecting variables

--- src/controller/file-uploader.js
+++ src/controller/file-uploader.js
@@ -1,12 +1,21 @@
 async function post(req, res, next) {
-    const response = `O pipeline do projeto File Uploader terminou!\n
-**Branch:** ${req.body.build.branch}\n
-**Author:** ${req.body.build.author.name}\n
-**Status:** ${req.body.build.state}\n
-**URL:** ${req.body.build.web_url}`
+    const info = req.body.build
+    let responseText = 'O pipeline do projeto File Uploader terminou!\n'
+    if (info.branch)  {
+        responseText += `**Branch:** ${info.branch}\n`
+    }
+    if (info.author && info.author.name)  {
+        responseText += `**Author:** ${info.author.name}\n`
+    }
+    if (info.state)  {
+        responseText += `**Status:** ${info.state}\n`
+    }
+    if (info.web_url)  {
+        responseText += `**URL:** ${info.web_url}`
+    }
 
     await axios.post('https://chat.involves.com/hooks/rzzpyfqsoinjixmtb3wn6og49a', {
-        text: response
+        text: responseText
     })
 
     res.writeHead(200)
