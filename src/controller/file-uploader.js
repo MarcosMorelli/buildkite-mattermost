2b170a8ee9a54f52022dc35f0f4afc5a912c8dcc
Morelli <marcos.morelli@involves.com>
2021-01-21 17:02:33 -0300

    mm webhook integration

--- src/controller/file-uploader.js
+++ src/controller/file-uploader.js
@@ -1,6 +1,16 @@
-async function post (req, res, next) {
-    console.log(req.body)
-    res.status(200).json({status: true})
+async function post(req, res, next) {
+    const response = `O pipeline do projeto File Uploader terminou!\n
+**Branch:** ${req.body.build.branch}\n
+**Author:** ${req.body.build.author.name}\n
+**Status:** ${req.body.build.state}\n
+**URL:** ${req.body.build.web_url}`
+
+    const res = await axios.post('https://chat.involves.com/hooks/rzzpyfqsoinjixmtb3wn6og49a', {
+        text: response
+    })
+
+    res.writeHead(200)
+    res.end()
 }
 
 module.exports = {
