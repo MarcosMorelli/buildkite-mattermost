e9a2964fecbbbf5be20db689c12343b7357ece36
Morelli <marcos.morelli@involves.com>
2021-01-21 16:35:34 -0300

    including body parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const bodyParser = require('body-parser')
 const compression = require('compression')
 const express = require('express')
 const controller = require('./controller')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(compression())
 
 app.post('/file-uploader', controller.FileUploader.post)
